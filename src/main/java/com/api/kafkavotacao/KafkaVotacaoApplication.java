4897857b6b6c567cd6a1ec9a57525820dff9929c
Alison8547 <alisonailson100@hotmail.com>
2023-04-21 15:28:39 -0300

    feat: estrutura base projeto

---
+++ src/main/java/com/api/kafkavotacao/KafkaVotacaoApplication.java
@@ -0,0 +1,13 @@
+package com.api.kafkavotacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaVotacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaVotacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/kafkavotacao/KafkaVotacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.kafkavotacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaVotacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
